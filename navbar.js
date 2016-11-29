4c6e8d08824eef7ce77933f667d30a1d26791d9f
Jusitn Martin <justinmartin2001@outlook.com>
2016-11-28 21:07:38 -0500

    navbar adjustments
    -adding box-shadow
    -fixed navbar
    -li hover and active css

---
+++ navbar.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  var displayHeight = $(".midDisplay").css( "height");
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > displayHeight) {
+            $(".topnav").css("box-shadow", "0 0 4px rgba(0,0,0,.14),0 4px 8px rgba(0,0,0,.28)");
+        } else {
+            $(".topnav").css("box-shadow", "0 0 0px rgba(0,0,0,0),0 0px 0px rgba(0,0,0,0)");
+        }
+    });
+});
+//box-shadow:0 0 4px rgba(0,0,0,.14),0 4px 8px rgba(0,0,0,.28);
