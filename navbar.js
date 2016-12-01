371ef0c7619bd3f0fe7347e2d0cdf4e37d594823
Jusitn Martin <justinmartin2001@outlook.com>
2016-11-30 21:05:30 -0500

    Navbar Responsivness mobile

--- navbar.js
+++ navbar.js
@@ -1,11 +1,8 @@
-$(document).ready(function () {
-  var displayHeight = $(".midDisplay").css( "height");
-    $(window).scroll(function () {
-        if ($(document).scrollTop() > displayHeight) {
-            $(".topnav").css("box-shadow", "0 0 4px rgba(0,0,0,.14),0 4px 8px rgba(0,0,0,.28)");
-        } else {
-            $(".topnav").css("box-shadow", "0 0 0px rgba(0,0,0,0),0 0px 0px rgba(0,0,0,0)");
-        }
-    });
-});
-//box-shadow:0 0 4px rgba(0,0,0,.14),0 4px 8px rgba(0,0,0,.28);
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
